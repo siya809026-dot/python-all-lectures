8714a1f590aa9ddd0ee32c46818317e56925d421
Siya <siya809026@gmail.com>
2026-09-09 01:32:01 +0530

    Add 4 notebook

---
+++ 3.py
@@ -0,0 +1,23 @@
+# check data type of the function
+print(type(1))
+# check data type of the function
+print(type("arya"))
+# check data type of the function
+print(type(True))
+# sytax
+# var_name = values
+a = 10
+print(type(a))
+
+# Mathematical operation with Varial Assignment
+
+a = 12
+b = 20
+
+print(a*b)
+print(a/b)
+print(a+b)
+print(a//b)
+
+# various ways of printing
+print("Hello")
